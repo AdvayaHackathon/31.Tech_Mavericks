f823ff5789d7e215fe84776574b7c2703e6273dc
vivek15072004 <125467368+vivek15072004@users.noreply.github.com>
2025-04-11 22:41:08 +0530

    All the aleret regarding files are completed

---
+++ Components/SVG/Nav2.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Nav2 = () => {
+  return (
+    <svg
+      className="brand-title"
+      width={110}
+      height={40}
+      viewBox="0 0 110 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <text
+        x=".5"
+        y="30"
+        font-family="Arial"
+        font-size="35"
+        font-weight="1800"
+        fill="#3D9662"
+      >
+        BETA
+      </text>
+      ;
+    </svg>
+  );
+};
+
+export default Nav2;
+
